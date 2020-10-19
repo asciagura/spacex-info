44948d6f3db8a3d09ec01f09671700fdfd91efda
andrea-sciagura <andrea@tappable.co.uk>
2020-10-19 18:49:05 +0100

    Migrated to coroutine

--- app/src/main/java/io/rockets/android/RocketDetailViewModel.kt
+++ app/src/main/java/io/rockets/android/RocketDetailViewModel.kt
@@ -5,6 +5,8 @@ import androidx.databinding.ObservableField
 import androidx.databinding.ObservableInt
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.viewModelScope
 import io.rockets.android.data.Launch
 import io.rockets.android.data.Rocket
 import io.rockets.android.net.ApiUtil
@@ -13,49 +15,55 @@ import io.rockets.android.repository.LaunchRepositoryImpl
 import io.rockets.android.repository.RocketRepository
 import io.rockets.android.repository.RocketRepositoryImpl
 import io.rockets.android.room.DatabaseUtil
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 
 class RocketDetailViewModel : ViewModel() {
-    private val launchesRepository: LaunchRepository = LaunchRepositoryImpl(ApiUtil.launchApi, DatabaseUtil.launchDao)
-    private val rocketRepository: RocketRepository = RocketRepositoryImpl(ApiUtil.rocketApi, DatabaseUtil.rocketsDao)
+    private val launchesRepository: LaunchRepository =
+        LaunchRepositoryImpl(ApiUtil.launchApi, DatabaseUtil.launchDao)
+    private val rocketRepository: RocketRepository =
+        RocketRepositoryImpl(ApiUtil.rocketApi, DatabaseUtil.rocketsDao)
     lateinit var rocketId: String
 
     val rocket = ObservableField<Rocket>()
     val loadingIndicatorVisibility = ObservableInt(View.GONE)
-    val launches = MutableLiveData<Map<Date, List<Launch>>>()
+    val launches = MutableLiveData<Map<Date?, List<Launch>>>()
 
-    private var launchesCached: Map<Date, List<Launch>>? = null
+    private var launchesCached: Map<Date?, List<Launch>>? = null
 
     fun setup(rocketId: String) {
         this.rocketId = rocketId
         loadingIndicatorVisibility.set(View.VISIBLE)
         rocketRepository.getRocket(rocketId) { }
-                .observeForever {
-                    rocket.set(it)
-                }
+            .observeForever {
+                rocket.set(it)
+            }
         load()
     }
 
     fun load(forceLoad: Boolean = false) {
         loadingIndicatorVisibility.set(View.VISIBLE)
         if (launchesCached == null || forceLoad)
-            launchesRepository.getRocketLaunches(rocketId) { loadingIndicatorVisibility.set(View.GONE) }
-                    .observeForever { list ->
+            viewModelScope.launch {
+                launchesRepository.getRocketLaunches(rocketId) { loadingIndicatorVisibility.set(View.GONE) }
+                    .asFlow().collect { list ->
                         launchesCached = groupAndSort(list).also {
-                            launches.value = it
+                            launches.postValue(it)
                         }
                     }
+            }
         else
             launches.value = launchesCached
     }
 
-    private fun groupAndSort(list: List<Launch>?): Map<Date, List<Launch>>? {
+    private fun groupAndSort(list: List<Launch>?): Map<Date?, List<Launch>>? {
         val dateFormat = SimpleDateFormat("yyyy", Locale.getDefault())
         return list?.sortedBy { launch -> launch.launchDateUtc }
-                ?.groupBy { launch ->
-                    dateFormat.parse(launch.launchYear)
-                }
+            ?.groupBy { launch ->
+                launch.launchYear.let { dateFormat.parse(it.orEmpty()) }
+            }
     }
 
 }
